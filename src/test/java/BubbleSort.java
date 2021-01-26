c3657f02aee12496e81414ab75222381cb714950
michael varchenko <Mishanjav2001@gmail.com>
2021-01-26 14:35:43 +0200

    src+

---
+++ src/test/java/BubbleSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arrayToSort = {5, 2, 1, 10, 50, 10};
+        for (int i = 0; i < arrayToSort.length - 1; i++) {
+            for (int j = 0; j < arrayToSort.length - 1; j++) {
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    int tmp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arrayToSort));
+    }
+}
+
